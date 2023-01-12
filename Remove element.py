0cb08431e1cb752bc848927266bfcebc60e977db
madhesh <madheshkumars03@gmail.com>
2023-01-12 21:18:49 +0530

    Added leetcode - easy challenge problem

---
+++ Remove element.py
@@ -0,0 +1,9 @@
+#Remove the  given element from the list and print the count of elements
+
+nums=list(map(int,input().split()))
+count=0
+for i in range(len(nums)):
+    if nums[i]!=val:
+        nums[count]=nums[i]
+        count+=1
+print(count)
