335c24f1e96c1e3a0e15800a5a233647dc71de27
Madhesh <madheshkumars03@gmail.com>
2023-02-10 22:39:17 +0530

    Added Leetcode medium Challenge.

--- add_two_num_linked_list.py
+++ add_two_num_linked_list.py
@@ -5,37 +5,30 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        res=ListNode(0)
+        res=ListNode()
+        temp=res
         carry=0
         arr=[]
-        while (l1 or l2) and res:
-            if l1 and not l2:
-                res.val=l1.val
+        while l1 or l2 or carry:
+            val=carry
+            if l1:
+                val+=l1.val
                 l1=l1.next
-                arr.append(res.val)
-            if not l1 and l2:
-                res.val=l2.val
+            if l2:
+                val+=l2.val
                 l2=l2.next
-                arr.append(res.val)
-            if l1 and l2:
-                res.val=l1.val+l2.val+carry
-                carry=res.val//10
-                res.val=res.val%10
-                arr.append(res.val)
-                l1=l1.next
-                l2=l2.next
-
-            res.next=ListNode(0)
-            res=res.next
-        if carry:
-            res.val=carry
-            arr.append(res.val)
-        return arr
+            carry=val//10
+            val=val%10
+            arr.append(val)
+            temp.next=ListNode(val)
+            temp=temp.next
+        return res.next,arr
 
 a=Solution()
 l1=ListNode(0,ListNode(0,ListNode(0,ListNode(1))))
 l2=ListNode(0,ListNode(2,ListNode(3)))
 print(a.addTwoNumbers(l1,l2))
+
 
 
 #input   3->2->4      423
